afc2abd9b92159f05e3d6424836b14c3df55eef9
Aqothy <aq0335@gmail.com>
2024-11-14 01:40:58 -0500

    more key maps

--- lua/aqothy/remap.lua
+++ lua/aqothy/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>sv", "<C-w>v")
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>")
 vim.api.nvim_set_keymap("v", "cy", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "wq", ":q<CR>", { noremap = true, silent = true })
