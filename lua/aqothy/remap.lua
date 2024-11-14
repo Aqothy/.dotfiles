148c3df6ce81fd5058b2eb789803c40690d2ab1a
Aqothy <aq0335@gmail.com>
2024-11-13 23:22:39 -0500

    more configs

--- lua/aqothy/remap.lua
+++ lua/aqothy/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>sv", "<C-w>v")
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>")
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "cy", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "wq", ":q<CR>", { noremap = true, silent = true })
