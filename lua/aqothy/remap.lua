d9525256e6a6ef86937a1b72487042ee8080ffdf
Aqothy <aq0335@gmail.com>
2024-11-14 02:38:47 -0500

    i swear final

--- lua/aqothy/remap.lua
+++ lua/aqothy/remap.lua
@@ -4,3 +4,4 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>")
 vim.api.nvim_set_keymap("v", "cy", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "wq", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
