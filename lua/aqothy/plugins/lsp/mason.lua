b01082a30606a0f1e3c9730333e640db48008b1c
Aqothy <aq0335@gmail.com>
2024-11-14 00:15:28 -0500

    trouble fix

--- lua/aqothy/plugins/lsp/mason.lua
+++ lua/aqothy/plugins/lsp/mason.lua
@@ -1,46 +1,47 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-    
-    local mason_tool_installer = require("mason-tool-installer")
-    
-    mason.setup()
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-        ensure_installed = {
-            "html",
-            "cssls",
-            "tailwindcss",
-            "lua_ls",
-            -- "emmet_ls",
-            "pyright",
-            "clangd",
-            "gopls",
-            "jdtls",
-            "texlab",
-            -- "typescript-language-server", -- just manually download on mason, dk why its broken
-        },
-    })
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
+		mason.setup()
 
-  end,
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				-- "emmet_ls",
+				"pyright",
+				"clangd",
+				"gopls",
+				"jdtls",
+				"texlab",
+				"sqls",
+				-- "typescript-language-server", -- just manually download on mason, dk why its broken
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
 }
+
