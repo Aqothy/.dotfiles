ce909de651928028c83817a749363eab8cca4f87
Aqothy <aq0335@gmail.com>
2024-11-17 21:35:46 -0500

    f

--- nvim/lua/aqothy/plugins/fugitive.lua
+++ nvim/lua/aqothy/plugins/fugitive.lua
@@ -2,36 +2,6 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-        local aqothy_fugitive = vim.api.nvim_create_augroup("aqothy_fugitive", {})
-
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = aqothy_fugitive,
-            pattern = "*",
-            callback = function()
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, opts)
-
-                -- rebase always
-                vim.keymap.set("n", "<leader>pm", function()
-                    vim.cmd.Git({'pull',  '--merge'})
-                end, opts)
-
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>po", ":Git push -u origin ", opts);
-            end,
-        })
-
-
         vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
         vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
         vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>", { desc = "Open Fugitive vertical diff" })
