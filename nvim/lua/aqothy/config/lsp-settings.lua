654c80c74b1e0843730bfa45853a57869239ee84
Aqothy <aq0335@gmail.com>
2025-05-17 23:31:11 -0400

    eslint fixall keymap

--- nvim/lua/aqothy/config/lsp-settings.lua
+++ nvim/lua/aqothy/config/lsp-settings.lua
@@ -17,6 +17,16 @@ M["eslint"] = {
 		workingDirectories = { mode = "auto" },
 		format = false,
 	},
+	keys = {
+		{
+			"n",
+			"<leader>fa",
+			"<cmd>LspEslintFixAll<cr>",
+			{
+				desc = "Fix all",
+			},
+		},
+	},
 }
 
 local jsts_config = {
